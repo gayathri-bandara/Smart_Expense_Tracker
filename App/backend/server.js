4db9bfba9e3abf6bd3ada485ccc154abfe2a11b7
gayathri-bandara <hwrbandara@gmail.com>
2026-05-07 13:51:12 +0530

    first commit

---
+++ App/backend/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API running...");
+});
+
+app.listen(5000, () => console.log("Server started"));
+``
